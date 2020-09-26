bc9786014d6841f0d44fa8aadbb025986017d66b
Alan Lee <alanleunglee@gmail.com>
2020-09-26 00:51:33 -0700

    feat(ws): payment handler stream poc

---
+++ websockets.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+
+type PaymentDetail struct {
+	PaymentPointer string `json:"paymentPointer"`
+	RequestID string `json:"requestId"`
+	Amount string `json:"amount"`
+	AssetCode string `json:"assetCode"`
+	AssetScale int `json:"assetScale"`
+	Receipt string `json:"receipt"`
+}
+
+
+var upgrader = websocket.Upgrader{}
+
+func payments(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Print("Upgrade:", err)
+		return
+	}
+
+	for {
+		var raw json.RawMessage
+		err := c.ReadJSON(&raw)
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+
+		var payDetails PaymentDetail
+
+		jsonErr := json.Unmarshal(raw, &payDetails)
+
+		if jsonErr != nil {
+			log.Println("json parse:", jsonErr)
+		}
+
+		log.Println(payDetails)
+	}
+}
