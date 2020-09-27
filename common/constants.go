c6e5f21d3b83859a4e50404c6b61cfe1a8d36fbb
Alan Lee <alanleunglee@gmail.com>
2020-09-27 00:29:13 -0700

    feat(constants): sql constants

--- common/constants.go
+++ common/constants.go
@@ -4,4 +4,9 @@ const (
 	REDIS_PORT     = "6379"
 	REDIS_HOST     = "localhost"
 	REDIS_PASSWORD = ""
+	POSTGRES_HOST = "localhost"
+	POSTGRES_PORT = "5432"
+	POSTGRES_USER = "postgres"
+	POSTGRES_PASS = "asdf1234"
+	POSTGRES_DBNAME = "payments"
 )
