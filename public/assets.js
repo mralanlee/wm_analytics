2b6254b78837057b699f86590a2577ad7261c1b1
Alan Lee <alanleunglee@gmail.com>
2020-09-26 22:10:04 -0700

    fix: revise event listener

--- public/assets.js
+++ public/assets.js
@@ -1,10 +1,17 @@
 const socket = new WebSocket('ws://localhost:3000/payments');
 
-socket.addEventListener('open', ev => {
-  document.monetization.addEventListener(
-    'monetizationprogress',
-    e => {
+// socket.addEventListener('open', ev => {
+// })
+
+document.monetization.addEventListener(
+  'monetizationprogress',
+  e => {
+    if (socket.readyState === 1) {
       socket.send(JSON.stringify(e.detail))
+    } else if (socket.readyState === 2 || socket.readyState === 3) {
+      console.log("WebSocket Connection is closed");
+    } else {
+      console.log("Connection is not ready", socket.readyState)
     }
-  )
-})
+  }
+)
