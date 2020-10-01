b1b7cebe334d5cb2699923abd31979189a583392
Alan Lee <alanleunglee@gmail.com>
2020-09-30 22:43:31 -0700

    fix: add path to ws payload

--- public/assets.js
+++ public/assets.js
@@ -3,11 +3,19 @@ const socket = new WebSocket('ws://localhost:3000/payments');
 // socket.addEventListener('open', ev => {
 // })
 
+const urlPath = window.location.pathname;
+
 document.monetization.addEventListener(
   'monetizationprogress',
   e => {
     if (socket.readyState === 1) {
-      socket.send(JSON.stringify(e.detail))
+      const { detail } = e;
+      const payload = {
+        ...detail,
+        urlPath,
+      };
+
+      socket.send(JSON.stringify(payload))
     } else if (socket.readyState === 2 || socket.readyState === 3) {
       console.log("WebSocket Connection is closed");
     } else {
