5144f87484843825bdd621074aaf54e605dc9cf1
Alan Lee <alan@coil.com>
2020-10-06 17:14:38 -0700

    feat(payments): handle capture

--- main.go
+++ main.go
@@ -8,7 +8,12 @@ import (
 func main() {
 	fs := http.FileServer(http.Dir("./public"))
 	http.Handle("/", fs)
+
+	// websockets implementation
 	http.HandleFunc("/payments", payments)
+
+	// if this works out better, remove the web sockets implementation
+	http.HandleFunc("/capture", capturePayments)
 	err := http.ListenAndServe(":3000", nil)
 
 	if err != nil {
