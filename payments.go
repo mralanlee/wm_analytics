65813fc18d22812c6dc31ca2ad2136b431b7408d
Alan Lee <alanleunglee@gmail.com>
2020-10-08 12:58:00 -0700

    feat(payments): logging and error handling

--- payments.go
+++ payments.go
@@ -2,31 +2,41 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/mralanlee/wm_analytics/consumers"
 )
 
 func capturePayments(w http.ResponseWriter, r *http.Request) {
+	log.Println("Incoming request")
+	if r.Method != http.MethodPost {
+		log.Fatal("Invalid http method")
+		http.Error(w, "Request method not supported", http.StatusMethodNotAllowed)
+		return
+	}
+
 	var p consumers.Payments
 
 	decoder := json.NewDecoder(r.Body)
 
 	if err := decoder.Decode(&p); err != nil {
+		log.Fatal(err)
 		http.Error(w, "Error reading request", http.StatusInternalServerError)
 		return
 	}
 
-	for _, v := range p.Payments {
-		go func(detail consumers.PaymentDetail) {
+	for _, v := range p.Details {
+		go func(detail *consumers.PaymentDetail) {
 			detail.Transform()
 			result := consumers.PostgresClient.Create(detail)
 
 			if result.Error != nil {
+				log.Fatal(result.Error)
 				http.Error(w, "Error processing transaction", http.StatusInternalServerError)
 				return
 			}
-		}(v)
+		}(&v)
 	}
 
 	w.WriteHeader(200)
