172da7f74b239111f3b507db2258994a9bae1dd5
Alan Lee <alanleunglee@gmail.com>
2020-10-08 16:39:25 -0700

    refactor: batch process with js calculation

--- public/assets.batched.js
+++ public/assets.batched.js
@@ -2,7 +2,22 @@ const urlPath = window.location.pathname;
 const BATCH = 5;
 const INTERVAL = 5000;
 let paymentStore = [];
-let SEND_PAYMENTS = false;
+
+window.setInterval(handler, INTERVAL)
+
+function calculateAmount(amount, assetScale) {
+  const value = parseInt(amount)
+  const scale = Math.pow(10, assetScale)
+
+  return value / scale
+}
+
+document.monetization.addEventListener(
+  'monetizationstart',
+  e => {
+    SEND_PAYMENTS = true
+  }
+)
 
 document.monetization.addEventListener(
   'monetizationprogress',
